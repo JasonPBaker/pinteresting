76f97fd64d49ca74f81e1a5ef5898af186d1ebc6
Jason Baker <jasontylerbaker@gmail.com>
2016-01-16 17:27:26 -0600

    Sign up and Log in button fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # devise_for :users
-  # devise_for :users
+  devise_for :users 
   root "pages#home"
   get "about" => "pages#about" #creates about_path
   get "products" => "pages#products"
